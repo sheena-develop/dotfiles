a11bd0f8b9f95e867c804d7cdeaa9d844fac7859
sheena-develop <sheena.develop@gmail.com>
2025-03-17 07:12:24 +0900

    ✨ feat: Neovimの設定の更新

--- neovim/nvim/lua/plugin-options/toggleterm.lua
+++ neovim/nvim/lua/plugin-options/toggleterm.lua
@@ -1,14 +1,9 @@
 -- https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#setup
 return {
-  --size = 100,
-  --open_mapping = [[<C-t>]],
-  hide_numbers = true,
-  shade_filetypes = {},
+  hide_numbers = false,
   shade_terminals = false,
-  shading_factor = 2,
-  start_in_insert = true,
+  start_in_insert = false,
   insert_mappings = true,
   persist_size = true,
-  --direction = "float",
   close_on_exit = true,
 }
